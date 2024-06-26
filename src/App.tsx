1c08febd5238ee9ab63e6cb5aa01004a906ca367
sunnysideup0w0v <723973972538@naver.com>
2024-06-27 08:52:10 +0900

    next-migration

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '../public/vite.svg';
+import './App.css';
+
+// next에서 이미지를 가져올 때에는 객체 형태로 가져오기 때문에 .src를 사용해서 수정해주어야한다.
+// package.json 파일의 속성을 next.js 속성으로 변경하여 run dev를 해주고 -> next-env.d.ts 신규 생성됨
+// 그 뒤에 속성 충돌 방지를 위해 vite-env.d.ts 파일 제거
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo.src} className='logo' alt='Vite logo' />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo.src} className='logo react' alt='React logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,11 +29,11 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Vite + React + TS',
+};
+
+// 주석된 값 = next.js에서 기본적으로 제공하여 기재하지 되지 않은 값.
+// svg -> app 디렉토리 바로 아래에 넣어두면 next.js에서 자동으로 favicon으로 인식한다.
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      {/* <head>
+        <meta charset='UTF-8' /> 
+        <link rel='icon' type='image/svg+xml' href='/vite.svg' /> 
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <title>Vite + React + TS</title> // nextMetadata를 통해 적용 가능 
+      </head> */}
+      <body>
+        <div id='root'>{children}</div>
+      </body>
+    </html>
+  );
+}
