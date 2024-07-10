ac32013f196f91ae2737d1cdee20e5d0b1444159
sunnysideup0w0v <723973972538@naver.com>
2024-07-10 13:55:05 +0900

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
 };
 
 // 주석된 값 = next.js에서 기본적으로 제공하여 기재하지 되지 않은 값.
-// svg -> app 디렉토리 바로 아래에 넣어두면 next.js에서 자동으로 favicon으로 인식한다.
+// svg -> app 디렉토리 바로 아래에 넣어두면 next.js에서 자동으로 favicon으로 인식한다. Test
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
